0d7c44cb0839b79fea3c6688d48e899da8dafc76
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-12-13 09:21:56 +0100

    Ajout menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('#menu');
+// Sélection des éléments
+const menuButton = document.querySelector('.header__menu-button');
+const menu = document.getElementById('menu');
+const menuCloseButton = document.querySelector('.header__menu-close');
 
-menuButton.addEventListener('click', () => {
-    const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
-    menuButton.setAttribute('aria-expanded', !isExpanded);
-    menu.setAttribute('aria-hidden', isExpanded);
-});
+// Fonction pour ouvrir/fermer le menu
+function toggleMenu() {
+    const isMenuOpen = menu.getAttribute('aria-hidden') === 'false'; // Vérifie si le menu est ouvert
+    menu.setAttribute('aria-hidden', isMenuOpen ? 'true' : 'false'); // Met à jour l'état `aria-hidden`
+    menuButton.setAttribute('aria-expanded', !isMenuOpen); // Met à jour l'état `aria-expanded`
+}
+
+// Écouteurs d'événements
+menuButton.addEventListener('click', toggleMenu); // Ouvrir le menu
+menuCloseButton.addEventListener('click', toggleMenu); // Fermer le menu
